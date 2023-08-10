830188a40485c3a8c09632e08f5601dea2d2d2a8
Konstantin Novosad <konstantus@live.ru>
2023-08-11 07:50:13 +0800

    ALG: expanded tests

--- trainings_1/a_conditioner_test.js
+++ trainings_1/a_conditioner_test.js
@@ -2,5 +2,6 @@ const { tableAssert } = require('../helpers/assertTable');
 const { solution } = require('./a_conditioner');
 
 tableAssert(solution, [
-  [['10 20', 'heat'], 20]
+  [['10 20', 'heat'], 20],
+  [['10 20', 'freeze'], 10],
 ]);
--- trainings_1/b_triangles_test.js
+++ trainings_1/b_triangles_test.js
@@ -4,5 +4,10 @@ const { solution, parseInput } = require('./b_triangles')
 const funcToTest = (...args) => solution(parseInput(...args));
 
 tableAssert(funcToTest, [
-  [['1', '2', '3'], 'NO']
+  [['3', '4', '5'], 'YES'],
+  [['4', '3', '5'], 'YES'],
+  [['5', '4', '3'], 'YES'],
+  [['5', '3', '4'], 'YES'],
+  [['1', '2', '3'], 'NO'],
+  [['1', '2', '7'], 'NO'],
 ]);
